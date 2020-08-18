bb13332fd33e85c0bc20486229117a452d9c7eb2
Foster917 <51181568+Foster917@users.noreply.github.com>
2020-08-18 11:36:51 -0500

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express"); // gives access to the express library by searching for electron in "node_modules"
 const app = express(); //creates an instance of the electron constructor that we name app
 
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+
 app.listen(8000, function () {
   console.log("server is running");
 });
